52972261bb812acc9d480ec25475894232bc5c2f
Sp0k <contact@gabsavard.com>
2025-08-20 09:42:27 -0300

    feat(clock): update to add seconds

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -17,21 +17,28 @@ const Clock = () => {
   const minutes = time.getMinutes();
   const hours = time.getHours();
 
+  const secondHandAngle = seconds * 6;
   const minuteHandAngle = minutes * 6 + seconds * 0.1; 
   const hourHandAngle = (hours % 12) * 30 + minutes * 0.5;
 
   return (
     <SpotlightCard className='w-full h-fit mb-6 drop-shadow-sm drop-shadow-gray-700' spotlightColor="rgba(147, 72, 217, 0.4)">
       <div id='Clock' className='mx-auto w-56 h-56 border border-white rounded-[50%] -rotate-90'>
+        <div id='center' className='absolute top-1/2 left-1/2 bg-white border border-white w-3 h-3 translate-x-[-40%] translate-y-[-40%] rounded-[50%]' />
+        <div
+          id="hour"
+          className="absolute top-1/2 left-1/2 origin-[0%_0%] w-1/3 h-[2px] bg-white/60"
+          style={{ transform: `rotate(${hourHandAngle}deg)` }}
+        />
         <div
           id="minutes"
-          className="absolute top-1/2 left-1/2 origin-[0%_0%] w-2/5 h-[2px] bg-gray-400 transition-transform duration-500 ease-in-out"
+          className="absolute top-1/2 left-1/2 origin-[0%_0%] w-2/5 h-[2px] bg-white/30"
           style={{ transform: `rotate(${minuteHandAngle}deg)` }}
         />
         <div
-          id="hour"
-          className="absolute top-1/2 drop-shadow-black drop-shadow-sm left-1/2 origin-[0%_0%] w-1/3 h-[2px] bg-white transition-transform duration-500 ease-in-out"
-          style={{ transform: `rotate(${hourHandAngle}deg)` }}
+          id="seconds"
+          className="absolute top-1/2 left-1/2 origin-[0%_0%] w-4/9 h-[2px] bg-white"
+          style={{ transform: `rotate(${secondHandAngle}deg)` }}
         />
       </div>
     </SpotlightCard>
